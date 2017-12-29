091336b7559a16dd4e3f565eef3b6de623c5f87a
Martin <martin.ky.cheng@pccw.com>
2017-12-29 11:12:11 +0800

    MOD: Can brows js and css by webapp. (w/ embedded tomcat)

--- src/main/java/com/pccw/immd/adminfunc/config/CustomWebMvcConfig.java
+++ src/main/java/com/pccw/immd/adminfunc/config/CustomWebMvcConfig.java
@@ -5,11 +5,13 @@ import com.pccw.immd.commons.validator.CompositeValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.Validator;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 /**
  * Created by pccw on 2017-05-18.
@@ -20,11 +22,17 @@ public class CustomWebMvcConfig extends WebMvcConfigurerAdapter {
 
     private static final Logger LOG = LoggerFactory.getLogger(CustomWebMvcConfig.class);
 
+//    @Value("")
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        registry.addResourceHandler("*.js").resourceChain(true);
-        registry.addResourceHandler("*.html").resourceChain(false);
+        registry.addResourceHandler("**/*.js", "**/*.css")
+                .addResourceLocations("/")
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
+
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
 
--- src/main/java/com/pccw/immd/adminfunc/config/WebSecurityConfig.java
+++ src/main/java/com/pccw/immd/adminfunc/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 /**
  * Created by jeff on 5/7/17.
@@ -15,17 +16,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/**").permitAll();
-//        http
-//                .csrf()
-//                .disable();
-//        http
-//                .headers().frameOptions().sameOrigin();
-//        http
-//                .authorizeRequests().antMatchers("/**").permitAll()
-//                .anyRequest().permitAll();
-//        http
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http
+                .csrf()
+                .disable();
+        http
+                .headers().frameOptions().sameOrigin();
+        http
+                .authorizeRequests().antMatchers("/**").permitAll()
+                .anyRequest().permitAll();
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
 }
