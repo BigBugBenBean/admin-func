f2e55d0724ce075b2149840560d97ebe7f75bb6a
Vincent <vincent.cy.leung@pccw.com>
2018-02-06 17:49:13 +0800

    ADD: jsGrid, Application Fee Enquiry

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -20,3 +20,79 @@ var eServices2SelectedSubCat = SUB_CAT_MENU.ENQUIRY;
 var smartics2SelectedSubCat = SUB_CAT_MENU.ENQUIRY;
 var rmSelectedSubCat = SUB_CAT_MENU.ENQUIRY;
 
+function onEServices2SubCatSelected(subCat) {
+    switch (subCat) {
+        case SUB_CAT_MENU.ENQUIRY: {
+            eServices2SelectedSubCat = SUB_CAT_MENU.ENQUIRY;
+            break;
+        }
+        case SUB_CAT_MENU.MAINTENANCE: {
+            eServices2SelectedSubCat = SUB_CAT_MENU.MAINTENANCE;
+            break;
+        }
+        case SUB_CAT_MENU.MANAGEMENT: {
+            eServices2SelectedSubCat = SUB_CAT_MENU.MANAGEMENT;
+            break;
+        }
+    }
+    $('#eServices2SubCat').html(eServices2SelectedSubCat);
+}
+
+function onSmartics2SubCatSelected(subCat) {
+    switch (subCat) {
+        case SUB_CAT_MENU.ENQUIRY: {
+            smartics2SelectedSubCat = SUB_CAT_MENU.ENQUIRY;
+            break;
+        }
+        case SUB_CAT_MENU.MAINTENANCE: {
+            smartics2SelectedSubCat = SUB_CAT_MENU.MAINTENANCE;
+            break;
+        }
+        case SUB_CAT_MENU.MANAGEMENT: {
+            smartics2SelectedSubCat = SUB_CAT_MENU.MANAGEMENT;
+            break;
+        }
+    }
+    $('#smartics2SubCat').html(smartics2SelectedSubCat);
+}
+
+function onRmSubCatSelected(subCat) {
+    switch (subCat) {
+        case SUB_CAT_MENU.ENQUIRY: {
+            rmSelectedSubCat = SUB_CAT_MENU.ENQUIRY;
+            break;
+        }
+        case SUB_CAT_MENU.MAINTENANCE: {
+            rmSelectedSubCat = SUB_CAT_MENU.MAINTENANCE;
+            break;
+        }
+        case SUB_CAT_MENU.MANAGEMENT: {
+            rmSelectedSubCat = SUB_CAT_MENU.MANAGEMENT;
+            break;
+        }
+    }
+    $('#rmSubCat').html(rmSelectedSubCat);
+}
+
+function onMainCatSelected(cat) {
+    switch (cat) {
+        case CAT_MENU.E_SERVICES_2: {
+            break;
+        }
+        case CAT_MENU.SMARTICS_2: {
+            break;
+        }
+        case CAT_MENU.RM: {
+            break;
+        }
+    }
+    // $.post({
+    //     url: '/menu',
+    // });
+}
+
+function updateNavTitle() {
+    $('#eServices2SubCat').html(eServices2SelectedSubCat);
+    $('#smartics2SubCat').html(smartics2SelectedSubCat);
+    $('#rmSubCat').html(rmSelectedSubCat);
+}
