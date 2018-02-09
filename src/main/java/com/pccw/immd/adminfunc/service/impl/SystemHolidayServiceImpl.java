da410cd0c55d2dff5134dfed02ae90b35da994b0
Martin <martin.ky.cheng@pccw.com>
2018-02-09 16:43:22 +0800

    ADD: System holiday service ready

---
+++ src/main/java/com/pccw/immd/adminfunc/service/impl/SystemHolidayServiceImpl.java
@@ -0,0 +1,35 @@
+package com.pccw.immd.adminfunc.service.impl;
+
+import com.pccw.immd.adminfunc.domain.SystemHoliday;
+import com.pccw.immd.adminfunc.dto.UpmsUser;
+import com.pccw.immd.adminfunc.repository.UmSystemHolidayRepository;
+import com.pccw.immd.adminfunc.service.SystemHolidayService;
+import com.pccw.immd.adminfunc.service.UmpsEndPointService;
+import com.pccw.immd.adminfunc.service.UmpsEndPointServiceWithHeader;
+import com.pccw.immd.adminfunc.service.UpmsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import ws.upms.immd.v1.ITIAppException;
+import ws.upms.immd.v1.ITISysException;
+import ws.upms.immd.v1.Iss3UserSignOnDTO;
+
+import java.util.List;
+
+@Service("systemHolidayService")
+public class SystemHolidayServiceImpl implements SystemHolidayService {
+
+    @Autowired
+    @Qualifier ("umSystemHolidayRepository.eservice2")
+    private UmSystemHolidayRepository umSystemHolidayRepository;
+
+    @Autowired
+    @Qualifier ("umpsEndPointServiceWithHeader")
+    private UmpsEndPointServiceWithHeader umpsServiceWithHeader;
+
+    @Override
+    public List<SystemHoliday> listAll() {
+        return umSystemHolidayRepository.findAllByOrderByHolidayDateDesc();
+    }
+}
