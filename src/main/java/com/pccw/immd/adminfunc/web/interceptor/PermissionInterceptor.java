cc32fab194864716d2914ff0cae52696e44aaadb
Martin <martin.ky.cheng@pccw.com>
2018-02-15 17:28:12 +0800

    ADD: Permission Denied URL Redirect

--- src/main/java/com/pccw/immd/adminfunc/web/interceptor/PermissionInterceptor.java
+++ src/main/java/com/pccw/immd/adminfunc/web/interceptor/PermissionInterceptor.java
@@ -1,12 +1,11 @@
 package com.pccw.immd.adminfunc.web.interceptor;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /** 
  ** 
@@ -23,36 +22,15 @@ public class PermissionInterceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
         throws Exception {
 
-        long startTime = System.currentTimeMillis();
-        request.setAttribute("PermissionInterceptor ... startTime", startTime);
-
-        request.setAttribute("startTime", startTime);
-        return true;
-    }
-
-    //after the handler is executed
-    public void postHandle(
-        HttpServletRequest request, HttpServletResponse response, 
-        Object handler, ModelAndView modelAndView)
-        throws Exception {
-        
-        if (request == null )
-            return;
-        
-        long startTime = (Long)request.getAttribute("startTime");
-        
-        long endTime = System.currentTimeMillis();
-
-        long executeTime = endTime - startTime;
-        
-        //modified the exisitng modelAndView
-        if (modelAndView != null)
-            modelAndView.addObject("executeTime",executeTime);
-        
-        //log it
-        if(LOG.isDebugEnabled()){
-            LOG.debug("[" + handler + "] executeTime : " + executeTime + "ms");
-            LOG.debug("PermissionInterceptor ... ");
+        String uri = request.getRequestURI();
+        /**
+         * TODO: Replace logic when access right mapping defined.
+         */
+        if (uri.indexOf("noPermission.do") > -1) {
+            LOG.info("No Permission");
+            response.sendRedirect(request.getContextPath() +"/AUTH/no-permission.do");
+            return false;
         }
+        return true;
     }
 }
