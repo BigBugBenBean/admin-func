de71987b2f1ec801e4a00910554d6f075db0aab0
Vincent <vincent.cy.leung@pccw.com>
2018-04-12 16:36:58 +0800

    MOD: dynamic menu functions

--- src/main/java/com/pccw/immd/adminfunc/web/interceptor/MenuInterceptor.java
+++ src/main/java/com/pccw/immd/adminfunc/web/interceptor/MenuInterceptor.java
@@ -21,8 +21,10 @@ public class MenuInterceptor extends HandlerInterceptorAdapter {
     
     private static final Logger LOG = LoggerFactory.getLogger(MenuInterceptor.class);
     public static final String MENU_ROOT_KEY = "ROOT";
+    public static final String FUNC_MENU_KEY = "MENU";
 
     private MenuService.MenuItem applicationMenu;
+    private MenuService.MenuItem funcMenu;
 
     @Autowired
     @Qualifier("menuService.eservices2")
@@ -34,6 +36,27 @@ public class MenuInterceptor extends HandlerInterceptorAdapter {
             applicationMenu = menuService.buildMenuTree();
         }
 
+        // TODO: dummy data only, need to get the list from DB depends on UM_GROUP's function list
+        if (funcMenu == null) {
+            funcMenu = menuService.buildMenuTree();
+            if (funcMenu.getChild().size() > 2) {
+                funcMenu.getChild().get(0).getChild().get(0).getChild().remove(0);
+                funcMenu.getChild().get(0).getChild().get(0).getChild().remove(0);
+
+                funcMenu.getChild().get(0).getChild().get(1).getChild().remove(0);
+                funcMenu.getChild().get(0).getChild().get(1).getChild().remove(0);
+                funcMenu.getChild().get(0).getChild().get(1).getChild().remove(0);
+
+                funcMenu.getChild().get(0).getChild().get(2).getChild().remove(0);
+                funcMenu.getChild().get(0).getChild().get(2).getChild().remove(0);
+
+                funcMenu.getChild().get(0).getChild().get(1).getChild().clear();
+
+            }
+        }
+
+
+        request.setAttribute( FUNC_MENU_KEY, funcMenu );
         request.setAttribute( MENU_ROOT_KEY, applicationMenu );
         return true;
     }
