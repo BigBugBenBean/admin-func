694129769dd685377121d66d37e13dfc19056f04
Martin <martin.ky.cheng@pccw.com>
2018-04-25 18:03:32 +0800

    MOD: Tested the json by thymeleaf

--- src/main/java/com/pccw/immd/adminfunc/web/controller/SurgeControlController.java
+++ src/main/java/com/pccw/immd/adminfunc/web/controller/SurgeControlController.java
@@ -1,36 +1,38 @@
 package com.pccw.immd.adminfunc.web.controller;
 
-import com.pccw.immd.adminfunc.web.interceptor.BreadcrumbInterceptor;
+import com.pccw.immd.adminfunc.dto.ApplicationFeeEnquiryDTO;
+import com.pccw.immd.adminfunc.dto.SurgeControlDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-
-import static com.pccw.immd.adminfunc.web.interceptor.BreadcrumbInterceptor.FUNC_ID_KEY;
+import java.util.HashMap;
 
 @Controller
 public class SurgeControlController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(MenuController.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SurgeControlController.class);
 
     @GetMapping(value = "/e-Services-2/maintenance/surgeControl.do")
-    public String surgeControlPage(HttpServletRequest request) {
-        request.setAttribute(FUNC_ID_KEY, BreadcrumbInterceptor.FUNC_ID.Surge_Control);
+    public String surgeControlPage(HttpServletRequest request,
+                                   @ModelAttribute("testJson") SurgeControlDto testJson){
+        testJson.setAddress("testing ---- Address");
+        testJson.setName("T_ name ");
         return "/eServices2/SurgeControl/surge-control";
     }
 
     @GetMapping(value = "/e-Services-2/maintenance/surgeControl_Edit.do")
     public String editSurgeControlPage(HttpServletRequest request) {
-        request.setAttribute(FUNC_ID_KEY, BreadcrumbInterceptor.FUNC_ID.Surge_Control);
         return "/eServices2/SurgeControl/edit-surge-control";
     }
 
     @PostMapping(value = "/e-Services-2/maintenance/surgeControl_Updated.do")
     public String updateSurgeControlPage(HttpServletRequest request) {
-        request.setAttribute(FUNC_ID_KEY, BreadcrumbInterceptor.FUNC_ID.Surge_Control);
         return "/eServices2/SurgeControl/surge-control-updated";
     }
 
